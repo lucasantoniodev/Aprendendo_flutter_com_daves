62b969cceb86b1bcf2b0d2898437b6b5b221e8ad
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-11 23:50:32 -0300

    Stateful widget e setState

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  /* 
-   * runApp - é responsável por executar a aplicação
-   * runApp - vai rodar um widget/application
-   */
-
-  runApp(const MyApp());
+  runApp(const MyApp(nome: 'Tecão'));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+/// Stateful widget tem 2 objetos:
+/// 1 - Widget: Responsável por criar o objeto de estado <state> e guardar dados não mutáveis
+/// 2 - Objeto de estado <state>: Responsável por criar o widget com método build e mostrar as variáveis mutáveis e não mutáveis.
+class MyApp extends StatefulWidget {
+  final String? nome;
+
+  const MyApp({Key? key, this.nome}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int salario = 7000;
+
+  void aumentaSalario(int valor) {
+    setState(() => salario += valor);
+  }
+
+  void diminuiSalario(int valor) {
+    setState(() => salario -= valor);
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Aplicativo olá mundo'),
-        ),
-        body: const Center(
-          child: Text(
-            'Olá mundãoo!!',
-            style: TextStyle(fontSize: 16, color: Colors.black),
-          ),
-        ),
-      ),
+          body: Center(
+        child: GestureDetector(
+            onTap: () {
+              // ignore: avoid_print
+              print('Clicou');
+              // ignore: avoid_print
+              print('Novo salario: $salario');
+              aumentaSalario(200);
+            },
+            onDoubleTap: () => diminuiSalario(200),
+            child: Text('O salário de ${widget.nome} é $salario')),
+      )),
     );
   }
 }
