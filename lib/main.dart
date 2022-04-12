9b05a5cc069dfb8afd07b08c4cf989330e69f762
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-12 17:15:42 -0300

    Montando calculadora Parte 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,15 +17,132 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Imagens'),
+          title: const Center(
+            child: Text('Calculadora'),
+          ),
         ),
-        body: Image.asset(
-          'assets/images/dog.webp',
-          width: double.infinity,
-          height: double.infinity,
-          fit: BoxFit.cover,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Text(
+                  'Número',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  'AC',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '<',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  '7',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '8',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '9',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '/',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  '4',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '5',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '6',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  'X',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  '1',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '2',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '3',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '-',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: const [
+                Text(
+                  '0',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '.',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '=',
+                  style: TextStyle(fontSize: 48),
+                ),
+                Text(
+                  '+',
+                  style: TextStyle(fontSize: 48),
+                ),
+              ],
+            ),
+            Text('Coluna 6'),
+          ],
         ),
       ),
     );
