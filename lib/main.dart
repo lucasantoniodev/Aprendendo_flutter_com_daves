aca30e31e6b7bae9b9d2da31c0bfbb84915096fc
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-12 12:12:42 -0300

    Usando imagens da pasta

--- lib/main.dart
+++ lib/main.dart
@@ -4,38 +4,26 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Stack APP'),
+          title: const Text('Imagens'),
         ),
-        body: Stack(
-          // fit: StackFit.expand,
-          // alignment: AlignmentDirectional.center, // Alinhar o primeiro em relação ao segundo
-          children: [
-            Container(
-              width: 200,
-              height: 200,
-              color: Colors.blue,
-            ),
-            Positioned(
-              // top: 20,
-              // left: 20,
-              // right: -30,
-              left: 50,
-              top: 50,
-              child: Container(
-                width: 100,
-                height: 100,
-                color: Colors.red,
-              ),
-            ),
-          ],
+        body: Image.asset(
+          'assets/images/dog.webp',
+          width: double.infinity,
+          height: double.infinity,
+          fit: BoxFit.cover,
         ),
       ),
     );
