dc9d6536558d0d408229be40bde515c1be0c53cc
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-12 01:37:52 -0300

    Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,48 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp(nome: 'Tecão'));
+  runApp(const MyApp());
 }
 
-/// Stateful widget tem 2 objetos:
-/// 1 - Widget: Responsável por criar o objeto de estado <state> e guardar dados não mutáveis
-/// 2 - Objeto de estado <state>: Responsável por criar o widget com método build e mostrar as variáveis mutáveis e não mutáveis.
 class MyApp extends StatefulWidget {
-  final String? nome;
-
-  const MyApp({Key? key, this.nome}) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
-  int salario = 7000;
-
-  void aumentaSalario(int valor) {
-    setState(() => salario += valor);
-  }
-
-  void diminuiSalario(int valor) {
-    setState(() => salario -= valor);
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          body: Center(
-        child: GestureDetector(
-            onTap: () {
-              // ignore: avoid_print
-              print('Clicou');
-              // ignore: avoid_print
-              print('Novo salario: $salario');
-              aumentaSalario(200);
-            },
-            onDoubleTap: () => diminuiSalario(200),
-            child: Text('O salário de ${widget.nome} é $salario')),
-      )),
+        appBar: AppBar(
+          title: const Text('Aplicativo exemplo Scaffold'),
+        ),
+        body: const Center(
+          child: Text(
+            'Meu aplicativo',
+            style: TextStyle(color: Colors.indigoAccent),
+          ),
+        ),
+      ),
     );
   }
 }
