dd1f3d3a36e45d55e2d42501d6c7f60facf7ac6e
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-13 15:48:28 -0300

    Widget Text e seus parâmetros

--- lib/main.dart
+++ lib/main.dart
@@ -9,92 +9,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const Tela1(),
-        '/tela2': (context) => const Tela2(),
-        '/tela3': (context) => const Tela3(),
-      },
-    );
-  }
-}
-
-class Tela1 extends StatelessWidget {
-  const Tela1({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Tela 1'),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text('Tela 1'),
-            onPressed: () => Navigator.pushNamed(context, '/tela2'),
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Tela2 extends StatelessWidget {
-  const Tela2({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.yellow,
-        appBar: AppBar(
-          title: const Text('Tela 2'),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: Wrap(
-           spacing: 10,
-            children: [
-              ElevatedButton(
-                child: const Text('Tela 1'),
-                onPressed: () => Navigator.pop(context),
-              ),
-              // const SizedBox(width: 40),
-              ElevatedButton(
-                child: const Text('Tela 3'),
-                onPressed: () => Navigator.pushNamed(context,'/tela3'),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class Tela3 extends StatelessWidget {
-  const Tela3({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.yellow,
-        appBar: AppBar(
-          title: const Text('Tela 3'),
-          backgroundColor: Colors.red,
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text('Voltar para tela 2'),
-            onPressed: () => Navigator.pop(context),
-          ),
+    return Center(
+      child: Text(
+        'Texto\nTexto2', // Adicionando outra linha de texto
+        textDirection:
+            TextDirection.ltr, // Texto da esquerda para direita (Padrão)
+        style: TextStyle(
+          height: 2.15, // DISTÂNCIA ENTRE OS TEXTOS !!!!!!! 
+          fontSize: 100, // Alterar tamanho do texto (px);
+          fontStyle: FontStyle.italic,
+          fontWeight: FontWeight.bold, // Alterar o peso/espessura do texto "negrito";
+          color: Colors.blue, // Cor do texto
+          backgroundColor: Colors.yellow.shade700,
+          decoration: TextDecoration.underline
         ),
       ),
     );
