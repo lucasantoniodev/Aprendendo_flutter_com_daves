4f7d67fd8085efd6f78b5f36f83833340302fb87
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-12 21:20:09 -0300

    Rotas Anônimas e Navigation push

--- lib/main.dart
+++ lib/main.dart
@@ -1,293 +1,63 @@
-// ignore_for_file: prefer_const_constructors, avoid_print
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Tela1(),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
-  String numero = '0';
-
-  double primeiroNumero = 0.0;
-
-  String operacao = '';
-
-  void calcular(String tecla) {
-    switch (tecla) {
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-      case '.':
-        setState(() {
-          numero += tecla;
-
-          if (!numero.contains('.')) {
-            int numeroInt = int.parse(numero);
-            numero = numeroInt.toString();
-          }
-        });
-        break;
-      case 'ac':
-        setState(() {
-          numero = '0';
-        });
-        break;
-      case '<':
-        setState(() {
-          if (numero.isNotEmpty) {
-          numero = numero.substring(0, numero.length - 1);
-          }
-
-        });
-        break;
-      case '+':
-      case '-':
-      case 'x':
-      case '/':
-        operacao = tecla;
-        primeiroNumero = double.parse(numero);
-        setState(() {
-          numero = '0';
-        });
-        break;
-      case '=':
-        double resultado = 0.0;
-
-        if (operacao == '/') {
-          if (double.parse(numero) * 1 == 0) {
-            print('Erro: divisão por zero');
-            return;
-          }
-        }
-
-        if (operacao == '+') {
-          resultado = primeiroNumero + double.parse(numero);
-        } else if (operacao == '-') {
-          resultado = primeiroNumero - double.parse(numero);
-        } else if (operacao == 'x') {
-          resultado = primeiroNumero * double.parse(numero);
-        } else if (operacao == '/') {
-          resultado = primeiroNumero / double.parse(numero);
-        }
-
-        String resultadoString = resultado.toString();
-        // 3.0
-
-        List<String> resultadoPartes = resultadoString.split('.');
-        print(resultadoPartes);
-
-        if (int.parse(resultadoPartes[1]) * 1 == 0) {
-          print('Parte fracionária zero');
-
-          setState(() {
-            numero = int.parse(resultadoPartes[0]).toString();
-          });
-        } else {
-          setState(() {
-            numero = resultado.toString();
-          });
-        }
-        break;
-      default:
-        break;
-    }
-  }
+class Tela1 extends StatelessWidget {
+  const Tela1({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Center(
-            child: Text('Calculadora'),
+          title: const Text('Tela 1'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: const Text('Tela 1'),
+            onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: ((context) => const Tela2()),
+              ),
+            ),
           ),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  numero,
-                  style: TextStyle(fontSize: 48),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GestureDetector(
-                  onTap: () => calcular('ac'),
-                  child: Text(
-                    'AC',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                Text(
-                  '',
-                  style: TextStyle(fontSize: 48),
-                ),
-                Text(
-                  '',
-                  style: TextStyle(fontSize: 48),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('<'),
-                  child: Image.asset('assets/images/backspace.png',width: 72,color: Colors.blue),
-                  ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GestureDetector(
-                  onTap: () => calcular('7'),
-                  child: Text(
-                    '7',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('8'),
-                  child: Text(
-                    '8',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('9'),
-                  child: Text(
-                    '9',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('/'),
-                  child: Text(
-                    '/',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GestureDetector(
-                  onTap: () => calcular('4'),
-                  child: Text(
-                    '4',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('5'),
-                  child: Text(
-                    '5',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('6'),
-                  child: Text(
-                    '6',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('x'),
-                  child: Text(
-                    'X',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GestureDetector(
-                  onTap: () => calcular('1'),
-                  child: Text(
-                    '1',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('2'),
-                  child: Text(
-                    '2',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('3'),
-                  child: Text(
-                    '3',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('-'),
-                  child: Text(
-                    '-',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                GestureDetector(
-                  onTap: () => calcular('0'),
-                  child: Text(
-                    '0',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('.'),
-                  child: Text(
-                    '.',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('='),
-                  child: Text(
-                    '=',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => calcular('+'),
-                  child: Text(
-                    '+',
-                    style: TextStyle(fontSize: 48),
-                  ),
-                ),
-              ],
-            ),
-            Text('Coluna 6'),
-          ],
+      ),
+    );
+  }
+}
+
+class Tela2 extends StatelessWidget {
+  const Tela2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.yellow,
+        appBar: AppBar(
+          title: const Text('Tela 2'),
+          backgroundColor: Colors.red,
+        ),
+        body: Center(
+          child: ElevatedButton(
+            child: const Text('Tela 2'),
+            onPressed: () => Navigator.pop(context),
+          ),
         ),
       ),
     );
