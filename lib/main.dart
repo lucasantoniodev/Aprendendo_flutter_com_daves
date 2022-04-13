8166e6eb18ca8b91b94992b3b64a78f5249e4683
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-13 16:34:50 -0300

    Aplicando font como tema em todo o projeto "theme"

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Texto\nTexto2', // Adicionando outra linha de texto
-        textDirection:
-            TextDirection.ltr, // Texto da esquerda para direita (Padrão)
-        style: TextStyle(
-          height: 2.15, // DISTÂNCIA ENTRE OS TEXTOS !!!!!!!
-          fontSize: 100, // Alterar tamanho do texto (px);
-          fontStyle: FontStyle.italic,
-          fontWeight:
-              FontWeight.bold, // Alterar o peso/espessura do texto "negrito";
-          color: Colors.blue, // Cor do texto
-          backgroundColor: Colors.yellow.shade700,
-          decoration: TextDecoration.underline,
-          fontFamily: 'Nasalization-rg', // Font do texto da pasta assets configurado no pubspec.yaml
-        ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData( // Definindo um tema padrão para todo seu projeto
+        primarySwatch: Colors.red,
+        fontFamily: 'Nasalization-rg', 
       ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Aplicativo'),
+          ),
+          body: const Center(
+            child: Text(
+              'Texto\nTexto2', // Adicionando outra linha de texto
+              textDirection:
+                  TextDirection.ltr, // Texto da esquerda para direita (Padrão)
+              style: TextStyle(
+                height: 2.15, // DISTÂNCIA ENTRE OS TEXTOS !!!!!!!
+                fontSize: 100, // Alterar tamanho do texto (px);
+                // fontStyle: FontStyle.italic,
+                // fontWeight: FontWeight
+                //     .bold, // Alterar o peso/espessura do texto "negrito";
+                // color: Colors.blue, // Cor do texto
+                // backgroundColor: Colors.yellow.shade700,
+                // decoration: TextDecoration.underline,
+              ),
+            ),
+          )),
     );
+
+    //
   }
 }
