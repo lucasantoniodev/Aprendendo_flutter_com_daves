15d96c8512dd535b3f6bf7ee72d6b0982cf8df9f
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-13 16:46:53 -0300

    Propriedas do tema para todo o aplicativo

--- lib/main.dart
+++ lib/main.dart
@@ -11,31 +11,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData( // Definindo um tema padrão para todo seu projeto
-        primarySwatch: Colors.red,
-        fontFamily: 'Nasalization-rg', 
+      theme: ThemeData(
+        // Definindo um tema padrão para todo seu projeto
+        primarySwatch: Colors.red, // Cor do tema
+
+        fontFamily: 'Nasalization-rg', // Font do tema
+
+        brightness: Brightness.light, // Brilho de fundo
+
+        primaryColor: Colors.blue,
+
+        appBarTheme: const AppBarTheme(
+          // Propriedade para personalizar o padrão do tema
+
+          backgroundColor: Colors.yellow,
+        ),
+
       ),
       home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Aplicativo'),
-          ),
-          body: const Center(
-            child: Text(
-              'Texto\nTexto2', // Adicionando outra linha de texto
-              textDirection:
-                  TextDirection.ltr, // Texto da esquerda para direita (Padrão)
-              style: TextStyle(
-                height: 2.15, // DISTÂNCIA ENTRE OS TEXTOS !!!!!!!
-                fontSize: 100, // Alterar tamanho do texto (px);
-                // fontStyle: FontStyle.italic,
-                // fontWeight: FontWeight
-                //     .bold, // Alterar o peso/espessura do texto "negrito";
-                // color: Colors.blue, // Cor do texto
-                // backgroundColor: Colors.yellow.shade700,
-                // decoration: TextDecoration.underline,
-              ),
-            ),
-          )),
+        // backgroundColor: Colors.indigoAccent, // Cor de fundo do scaffold
+        
+        appBar: AppBar(title: const Text('Aplicativo'),
+        
+        backgroundColor: Colors.green,
+
+        ),
+      ),
     );
 
     //
