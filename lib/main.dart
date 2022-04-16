ffbde3beb3973ae1846b1377000bbdc3899df0b5
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-16 16:20:53 -0300

    Ler Dados JSON no Flutter via HTTP no Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,21 @@
-// ignore_for_file: invalid_return_type_for_catch_error
-
 import 'dart:async';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<void> main() async {
-  // SITE + PÁGINA + PARÂMETROS
-  final uriComRetorno404 = Uri.https(
-      'www.example.com', '/api', {'q': 'flutter', 'parametro2': 'valor'});
-  print(uriComRetorno404);
+  Uri uri = Uri.https('jsonplaceholder.typicode.com', '/todos/1');
 
-  final uri = Uri.https('www.example.com', '');
   final future = http.get(uri);
 
   future.then((response) {
     if (response.statusCode == 200) {
-      print('Página carregada com sucesso!');
-      print(response.body); // Código fonte HTML  da página
-    } else if (response.statusCode == 301) {
-      print('Página movida permanentemente');
-    } else if (response.statusCode == 404) {
-      print('Página não encontrada');
-    } else if (response.statusCode == 500) {
-      print('Erro interno no servidor');
+      print('Página carregada, OK!');
+
+      Map<String, dynamic> dados = json.decode(response.body);
+      print(dados);
+      print(dados['title']);
+    } else {
+      print('erro');
     }
   });
-
-  future.catchError((onError) => print('Erro!'));
-  // Quando terminar a requisição
-  future.whenComplete(() => print('Future completo!'));
 }
