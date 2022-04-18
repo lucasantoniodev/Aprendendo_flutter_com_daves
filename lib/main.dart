c0fcfae4d20e754e4749b089dc4cb5afbdaad211
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-18 02:28:32 -0300

    Header e usuário no menu drawer (menu lateral)

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,31 @@ class _MyAppState extends State<MyApp> {
             padding: const EdgeInsets.only(top: 40.0),
             child: Column(
               children: [
+                
+                UserAccountsDrawerHeader(
+                  currentAccountPicture: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Icecat1-300x300.svg/1200px-Icecat1-300x300.svg.png'),
+                  accountName: Text('Teco'),
+                  accountEmail: Text('email@email.com'),
+                ),
+                
+                
+                // DrawerHeader(
+                //   child: Text('Header'),
+                // ),
                 ListTile(
-                  leading: Icon(Icons.attach_money, color: Colors.red,),
+                  leading: Icon(
+                    Icons.attach_money,
+                    color: Colors.red,
+                  ),
                   title: Text('Menu Lateral'),
                   subtitle: Text('Selecione a opção nº 1'),
                   onTap: () => print('Clicou'),
                 ),
-                 ListTile(
-                  leading: Icon(Icons.edit, color: Colors.blue,),
+                ListTile(
+                  leading: Icon(
+                    Icons.edit,
+                    color: Colors.blue,
+                  ),
                   title: Text('Editar cadastro'),
                   subtitle: Text('Alterar dados do usuário'),
                   onTap: () => print('Clicou'),
