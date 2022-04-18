68f913729ad4f68cb9fc8b5df69bc88b01710906
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-18 00:41:53 -0300

    Menu inferior BottomNavigationBar e ButtomNavigationBarItems

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,55 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/utils/listar_produtos.dart';
 
-void main() async {
-  runApp(const ListarProduto());
+void main() {
+  runApp(const MyApp());
 }
 
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        endDrawer: Container(child: Text('iae')),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: 0,
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Colors.blue[900],
+          fixedColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'Página inicial',
+              
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              label: 'Pesquisa',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: 'Perfil',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Configurações',
+            ),
+          ],
+        ),
+        appBar: AppBar(
+          title: Text('Aplicativo exemplo BottomNavigatorBar'),
+        ),
+        body: Center(
+          child: Text('Meu app'),
+        ),
+      ),
+    );
+  }
+}
