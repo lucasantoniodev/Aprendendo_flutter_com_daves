7c501b5b2ab7003632c9ff03435de2263eac0c3a
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-17 22:46:26 -0300

    Formatando Dados no ListView e listTile, imagem do produto

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class _MyAppState extends State<MyApp> {
 
     var response = await http.get(
         uri); // A requisição .get retorna uma String, por isso precisamos converter para json no próximo método;
-    print(response.body.runtimeType); // Tipo do retorno "String".
+    // print(response.body.runtimeType); // Tipo do retorno "String".
 
     var dados = json.decode(response
         .body); // Transformando uma lista/objeto String para Lista de objeto/map;
@@ -50,12 +50,11 @@ class _MyAppState extends State<MyApp> {
     return produtos;
   }
 
-  
   @override
   Widget build(BuildContext context) {
-  
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(brightness: Brightness.dark),
       home: Scaffold(
         appBar: AppBar(
           title: Text('List View'),
@@ -70,7 +69,37 @@ class _MyAppState extends State<MyApp> {
                   itemCount: snapshot.data.length,
                   itemBuilder: (context, index) {
                     return ListTile(
-                      title: Text('${snapshot.data[index].descricao}'),
+                      leading: Image.network(
+                          'https://findicons.com/files/icons/1606/128x128_icons_6/128/apple.png'),
+                      title: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '${snapshot.data[index].produto}',
+                              style:
+                                  TextStyle(fontSize: 20, color: Colors.yellow),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    'R\$ ${snapshot.data[index].preco}',
+                                    style: TextStyle(
+                                      color: Colors.red,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      subtitle: Text('${snapshot.data[index].descricao.substring(0,30)}...'),
                       onTap: () {
                         print(
                             'O produto selecionado foi ${snapshot.data[index]}, na posição $index');
