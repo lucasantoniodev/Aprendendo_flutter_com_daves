11dea9cb26479418b294247da115b89a9fc71b6b
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-18 23:49:14 -0300

    Gerenciamento de estado com provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,32 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application/page/home_page.dart';
-import 'package:flutter_application/page/profile.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    Provider(create: (context) => Pessoa(22, 'Teco'), child: MyApp()),
+  );
 }
 
-class MyApp extends StatefulWidget {
+class Pessoa {
+  String nome = 'João';
+  int idade = 30;
+
+  Pessoa(this.idade, this.nome);
+}
+
+class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        drawer: Builder(
-          builder: (context) => Drawer(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 40.0),
-              child: Column(
-                children: [
-                  UserAccountsDrawerHeader(
-                    currentAccountPicture: Image.network(
-                        'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Icecat1-300x300.svg/1200px-Icecat1-300x300.svg.png'),
-                    accountName: Text('Teco'),
-                    accountEmail: Text('email@email.com'),
-                  ),
-                  ListTile(
-                    leading: Icon(
-                      Icons.attach_money,
-                      color: Colors.red,
-                    ),
-                    title: Text('Página inicial'),
-                    subtitle: Text('Conteúdo principal'),
-                    onTap: () {
-                      // Pop para fechar o menu
-                      Navigator.of(context).pop();
-
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (contex) {
-                        return HomePage();
-                      }));
-                    },
-                  ),
-                  ListTile(
-                    leading: Icon(
-                      Icons.edit,
-                      color: Colors.blue,
-                    ),
-                    title: Text('Editar cadastro'),
-                    subtitle: Text('Alterar dados do usuário'),
-                    onTap: () {
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) {
-                        return Profile();
-                      }));
-                    },
-                  ),
-                  Text('Option 1'),
-                  Text('Option 2'),
-                ],
-              ),
-            ),
-          ),
-        ),
-        appBar: AppBar(
-          title: Text('Aplicativo Menu Drawer'),
-        ),
+        appBar: AppBar(title: Text('Exemplo Provider')),
         body: Center(
-          child: Text('Página do usuário'),
-        ),
+            child: Text(
+          '${context.select((Pessoa p) => p.nome )} tem ${context.select((Pessoa p) => p.idade )} anos de idade',
+          style: TextStyle(fontSize: 30),
+        )),
       ),
     );
   }
