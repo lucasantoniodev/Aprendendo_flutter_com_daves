e3bf440dc24bc5891786faed448d72c4fdc290e2
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-19 11:58:05 -0300

    Usando Consumer no Provider, facilitando o consumo

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    ChangeNotifierProvider(create: (context) => Pessoa(22, 'Teco'), child: MyApp()),
+    // Provedor dinámico
+    ChangeNotifierProvider(
+        create: (context) => Pessoa(22, 'Teco'), child: MyApp()),
   );
 }
 
+// Classe com o modelo dos dados e com características da classe dinámica ChangeNotifier
 class Pessoa with ChangeNotifier {
   String nome = 'João';
   int idade = 30;
@@ -19,25 +22,29 @@ class Pessoa with ChangeNotifier {
   }
 }
 
-
-// Utilizar um provoder permite que uma classe Statelesse que não tem alterações passe a ter já que o está está sendo gerenciado pelo provider.
+// Utilizar um provoder permite que uma classe Stateless que não tem alterações passe a ter já que o está está sendo gerenciado pelo provider.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+        // Widget responsável por facilitar o consumo do provider, sem precisar escrever tanto para acessar os parâmetros
+        home: Consumer<Pessoa>(
+      builder: (context, pessoa, child) => Scaffold(
         appBar: AppBar(title: Text('Exemplo Provider')),
         body: Center(
-            child: Text(
-          '${context.select((Pessoa p) => p.nome)} tem ${context.select((Pessoa p) => p.idade)} anos de idade',
-          style: TextStyle(fontSize: 30),
-        )),
+          child: Text(
+            // Consumer      // Modelo antigo, sem Consumer
+            '${pessoa.nome} tem ${context.select((Pessoa p) => p.idade)} anos de idade',
+            style: TextStyle(fontSize: 30),
+          ),
+        ),
         floatingActionButton: FloatingActionButton(
-            onPressed: () =>
-                Provider.of<Pessoa>(context, listen: false).incrementAge()),
+          onPressed: () => pessoa.incrementAge(),
+          // Antigo: Provider.of<Pessoa>(context, listen: false).incrementAge()),
+        ),
       ),
-    );
+    ));
   }
 }
