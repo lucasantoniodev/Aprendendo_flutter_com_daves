eec790829cce328245caca5388739afa75e82a67
lucasantoniodev <lucasantoniodev@gmail.com>
2022-04-19 00:03:22 -0300

    Notify Listeners com ChangeNotifierProvider

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,24 @@ import 'package:provider/provider.dart';
 
 void main() {
   runApp(
-    Provider(create: (context) => Pessoa(22, 'Teco'), child: MyApp()),
+    ChangeNotifierProvider(create: (context) => Pessoa(22, 'Teco'), child: MyApp()),
   );
 }
 
-class Pessoa {
+class Pessoa with ChangeNotifier {
   String nome = 'João';
   int idade = 30;
 
   Pessoa(this.idade, this.nome);
+
+  void incrementAge() {
+    this.idade++;
+    notifyListeners();
+  }
 }
 
+
+// Utilizar um provoder permite que uma classe Statelesse que não tem alterações passe a ter já que o está está sendo gerenciado pelo provider.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -24,9 +31,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: Text('Exemplo Provider')),
         body: Center(
             child: Text(
-          '${context.select((Pessoa p) => p.nome )} tem ${context.select((Pessoa p) => p.idade )} anos de idade',
+          '${context.select((Pessoa p) => p.nome)} tem ${context.select((Pessoa p) => p.idade)} anos de idade',
           style: TextStyle(fontSize: 30),
         )),
+        floatingActionButton: FloatingActionButton(
+            onPressed: () =>
+                Provider.of<Pessoa>(context, listen: false).incrementAge()),
       ),
     );
   }
